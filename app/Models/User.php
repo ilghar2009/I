90bfe70aaedf3f1bfcd1e84f8d8907601c59a91c
ilghar2009 <ilghar.ebrahimi.1388@gmail.com>
2026-07-22 12:37:48 +0330

    relation eloquent user

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\AuthController\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
@@ -51,5 +52,20 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+
+    //RelationShips model
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class, 'userId');
+    }
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'userId');
+    }
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class, 'userId');
+    }
 
 }
